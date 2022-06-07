3cb163d83c905dd9d73031bb667c9fe32cc845c3
guilhermerenna <guilhermerenna@gmail.com>
2022-06-07 19:06:22 -0400

    Starting project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3030;
+
+app.get("/", (req, res) => {
+    res.json({message: `howdy ho, bitches! I'm running on port ${PORT}`});
+})
+
+app.listen(PORT, () => {
+    console.log(`Howdy ho!`);
+})
